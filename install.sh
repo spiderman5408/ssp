f96d8485221ab12f030dcf99e094cf18aa1409cc
Colette Contreras <ColetteContreras@outlook.com>
2020-10-05 15:47:56 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@
 CUR_VER=""
 NEW_VER=""
 ARCH=""
-VDIS="64"
+VDIS="amd64"
 ZIPFILE="/tmp/ssp/ssp.zip"
 ssp_RUNNING=0
 VSRC_ROOT="/tmp/ssp"
@@ -84,32 +84,6 @@ done
 colorEcho(){
     COLOR=$1
     echo -e "\033[${COLOR}${@:2}\033[0m"
-}
-
-sysArch(){
-    ARCH=$(uname -m)
-    if [[ "$ARCH" == "i686" ]] || [[ "$ARCH" == "i386" ]]; then
-        VDIS="32"
-    elif [[ "$ARCH" == *"armv7"* ]] || [[ "$ARCH" == "armv6l" ]]; then
-        VDIS="arm"
-    elif [[ "$ARCH" == *"armv8"* ]] || [[ "$ARCH" == "aarch64" ]]; then
-        VDIS="arm64"
-    elif [[ "$ARCH" == *"mips64le"* ]]; then
-        VDIS="mips64le"
-    elif [[ "$ARCH" == *"mips64"* ]]; then
-        VDIS="mips64"
-    elif [[ "$ARCH" == *"mipsle"* ]]; then
-        VDIS="mipsle"
-    elif [[ "$ARCH" == *"mips"* ]]; then
-        VDIS="mips"
-    elif [[ "$ARCH" == *"s390x"* ]]; then
-        VDIS="s390x"
-    elif [[ "$ARCH" == "ppc64le" ]]; then
-        VDIS="ppc64le"
-    elif [[ "$ARCH" == "ppc64" ]]; then
-        VDIS="ppc64"
-    fi
-    return 0
 }
 
 downloadssp(){
@@ -348,7 +322,6 @@ main() {
     [[ "$CHECK" == "1" ]] && checkUpdate && return
     [[ "$REMOVE" == "1" ]] && remove && return
     
-    sysArch
     # extract local file
     if [[ $LOCAL_INSTALL -eq 1 ]]; then
         colorEcho ${YELLOW} "Installing ssp via local file. Please make sure the file is a valid ssp package, as we are not able to determine that."
